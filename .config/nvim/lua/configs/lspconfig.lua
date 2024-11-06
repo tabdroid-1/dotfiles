1c7662bc0c6356e0b578244351fd17ad20fa81d4
tabdroid-1 <tabdroid.official@gmail.com>
2024-11-07 00:53:51 +0100

    nvim update and clean up

---
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,43 @@
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
+
+local lspconfig = require "lspconfig"
+local util = require "lspconfig/util"
+
+-- EXAMPLE
+local servers = { "html", "cssls" }
+local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+
+lspconfig.rust_analyzer.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  filetypes = {"rust"},
+  root_dir = util.root_pattern("Cargo.toml"),
+  settings = {
+    ['rust_analyzer'] = {
+      cargo = {
+        allFeatures = true,
+      },
+    },
+  },
+}
+
+lspconfig.clangd.setup {
+  -- on_attach = nvlsp.on_attach,
+  -- on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+    cmd = {
+        "clangd",
+        "--header-insertion=never",
+    },
+}
---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,28 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+function toggleCppHpp()
+  local current_file = vim.fn.expand "%"
+  local file_extension = vim.fn.expand "%:e"
+
+  if file_extension == "cpp" then
+    local hpp_file = vim.fn.expand "%:r" .. ".h"
+    vim.cmd("edit " .. hpp_file)
+  elseif file_extension == "h" then
+    local cpp_file = vim.fn.expand "%:r" .. ".cpp"
+    vim.cmd("edit " .. cpp_file)
+  else
+    print "Unsupported file extension"
+  end
+end
+
+-- vim.api.nvim_set_keymap("n", "<Tab>", ":lua toggleCppHpp()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Tab>", ":lua toggleCppHpp()<CR>", { noremap = true, silent = true })
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
