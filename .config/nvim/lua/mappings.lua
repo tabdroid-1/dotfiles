e78d6599351362f499201861c8a1a99b4c7a3620
tabdroid-1 <tabdroid.official@gmail.com>
2024-12-04 17:09:01 +0100

    gitignore and update

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,6 +6,8 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("v", "p", "P")
+map("v", "P", "p")
 
 function toggleCppHpp()
   local current_file = vim.fn.expand "%"
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,6 +6,7 @@ o.shiftwidth = 4
 o.smartindent = true
 o.tabstop = 4
 o.softtabstop = 4
+o.wrap = false
 
 o.commentstring = "// %s"
 
